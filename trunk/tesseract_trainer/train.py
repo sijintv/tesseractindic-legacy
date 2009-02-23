5db51c1edb7b200e567ffe48134e0a496577df1b
debayanin <debayanin@8c7c788f-554f-0410-aad6-efc5f5ac1a20>
2009-02-23 05:27:50 +0000

    the last commit did not work as expected, hope this one does

---
+++ trunk/tesseract_trainer/train.py
@@ -0,0 +1,44 @@
+import os
+import string
+
+def train():
+    print "in train"
+    dir="images/"
+    for t in os.walk(dir):
+        for f in t[2]:
+            img=f.split('.')[0]
+            print img
+            image=img+".tif"
+            box=img+".box"
+            exec_string1="tesseract "+dir+image+" junk nobatch box.train"
+            #print exec_string1
+            exit_code1=os.system(exec_string1) #creation of tr files done
+            
+    #now begins clustering
+    
+    exec_string2="mftraining"
+    exec_string3="cntraining"
+    
+    for t in os.walk(dir):
+        for f in t[2]:
+            if(f.split('.')[1]=="tr"):
+                exec_string2+=" "+dir+f+" "
+                exec_string3+=" "+dir+f+" "
+    exit_code2=os.system(exec_string2)
+    exit_code3=os.system(exec_string3)
+    #clustering ends
+    
+    #unicharset_extractor begins
+    exec_string4="unicharset_extractor"
+    for t in os.walk(dir):
+        for f in t[2]:
+            if(f.split('.')[1]=="box"):
+                exec_string4+=" "+dir+f
+                print exec_string4
+    exit_code4=os.system(exec_string4)
+    #unicharset_extractor ends
+            
+            
+            
+            
+    
