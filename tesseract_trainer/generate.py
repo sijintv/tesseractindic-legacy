6ae8d642aeeb6328a98bd1d416cee70cfd5f35df
debayanin <debayanin@8c7c788f-554f-0410-aad6-efc5f5ac1a20>
2009-04-29 23:19:11 +0000

    Now training image includes degraded character samples too

--- tesseract_trainer/generate.py
+++ tesseract_trainer/generate.py
@@ -28,44 +28,32 @@ def expand(temp_bbox):
 
 def draw(lang,font_name,font,fsz,alphabets): # language, font file name, font full path, font size, characters
     """ Generates tif images and box files"""
-    
+   
     image_dir=lang+"."+"images"
     if(os.path.exists(image_dir)):
         pass
     else:
         os.mkdir(image_dir)
-        
+       
     #Using a font
     #font= ImageFont.truetype(font,fsz)
-    filecount=0 #for different filenames for the images
-    filename1=image_dir+"/"+"image"+font_name.split('.')[0]+str(filecount)+".png"
-    filename2=image_dir+"/"+"image"+font_name.split('.')[0]+str(filecount)+".tif"
-    #boxfile=image_dir+"/"+"image"+font_name.split('.')[0]+str(filecount)+".box"
     boxfile=image_dir+"/"+"bigimage.box"
     f=open(boxfile,"w")
      
     bigimage=Image.new("L",(500,500),255)
     bigdraw=ImageDraw.Draw(bigimage)
     x=y=10
-    
+   
     for akshar in alphabets:
         akshar.strip() #remove nasty characters
-        
-        #I shall now create an image with black bgc and white font color. One 
+       
+        #I shall now create an image with black bgc and white font color. One
         #getbbox() determines the bounding box values I shall invert the image.
         #This has to be done since getbbox() only finds bounding box values for
-        #non-zero pixels (read as white), but tesseract-ocr runs on the exact 
+        #non-zero pixels (read as white), but tesseract-ocr runs on the exact
         #opposite bgc fgc combination. Contact debayanin@gmail.com.
-        
-        """temp_image=Image.new("L",(fsz*3,fsz*2)) #This is an image with a black background
-        #temp_image.show()
-        draw = ImageDraw.Draw(temp_image)
-        draw.text((fsz,5), unicode(akshar,'UTF-8'), font=font,fill=255) #draw the aksharas. fill=255 means white ink.
-        #temp_image.show()
-        original_bbox=temp_image.getbbox() #get the bounding box of the black pixels relative to the small image
-        symbol_image=ImageChops.invert(temp_image) #symbol_image is the black-on-white symbol image
-        draw= ImageDraw.Draw(symbol_image)      #Lets transfer "draw" to the new image"""
-        
+      
+       
         #The lines below are pango/cairo code
         surface = cairo.ImageSurface(cairo.FORMAT_A8, fsz*3, fsz*2)
         context = cairo.Context(surface)
@@ -83,67 +71,59 @@ def draw(lang,font_name,font,fsz,alphabets): # language, font file name, font fu
         position = (10,10)#(width/2.0 - w/2.0, height/2.0 - h/2.0)
         context.move_to(*position)
         pc.show_layout(layout)
-        surface.write_to_png(filename1)
+        surface.write_to_png("pango.png")
 
-	#Here we open the generated image using PIL functions
-	temp_image=Image.open(filename1) #black background, white text
-	draw = ImageDraw.Draw(temp_image)
-	bbox = temp_image.getbbox()
-	deltax=bbox[2]-bbox[0]
-	deltay=bbox[3]-bbox[1]
+        #Here we open the generated image using PIL functions
+        temp_image=Image.open("pango.png") #black background, white text
+        draw = ImageDraw.Draw(temp_image)
+        bbox = temp_image.getbbox()
+        deltax=bbox[2]-bbox[0]
+        deltay=bbox[3]-bbox[1]
 
-	
-	print bbox
-	new_image=temp_image.crop(bbox)
-	#temp_image=temp_image.load()
-	inverted_image = ImageChops.invert(new_image) #White background, black text
+       
+        print bbox
+        new_image=temp_image.crop(bbox)
+        #temp_image=temp_image.load()
+        inverted_image = ImageChops.invert(new_image) #White background, black text
 
-	bigimage.paste(inverted_image,(x,y))
-	bigbox=(x,y,x+deltax,y+deltay)
-	print bigbox
-	draw=ImageDraw.Draw(bigimage)
-	#draw.rectangle(bigbox,None,100)
-	x=bigbox[2]+2
-	if x>450:
+        bigimage.paste(inverted_image,(x,y))
+        bigbox=(x,y,x+deltax,y+deltay)
+        print bigbox
+        draw=ImageDraw.Draw(bigimage)
+        #draw.rectangle(bigbox,None,100)
+        x=bigbox[2]+2
+        if x>450:
             x=10; y=y+35
 
-	#draw= ImageDraw.Draw(inverted_image)      #Lets transfer "draw" to the new image
-	#draw.rectangle(bigbox,None,300) #draw the bounding box
-	#inverted_image.save(filename2,"TIFF")#save the symbol to a file
-	os.unlink(filename1) #delete the pango generated png
+        os.unlink("pango.png") #delete the pango generated png
 
-	line=akshar+" "+str(bigbox[0])+" "+str(500-(bigbox[1]+deltay))+" "+str(bigbox[2])+" "+str(500-(bigbox[3]-deltay)) # this is the line to be added to the box file
-	f.write(line+'\n')
-	#boxfile=image_dir+"/"+"image"+font_name.split('.')[0]+str(filecount)+".box"
-	#boxfile=image_dir+"/"+"bigimage.box"
-        #f.close()
-        #f=open(boxfile,"w") #open new file
+        line=akshar+" "+str(bigbox[0])+" "+str(500-(bigbox[1]+deltay))+" "+str(bigbox[2])+" "+str(500-(bigbox[3]-deltay)) # this is the line to be added to the box file
+        f.write(line+'\n')
 
-	#distort.distort(filename2,bbox,fsz,akshar)
+	#degrade code starts
+	strip=[deltax*.2,deltax*.4,deltax*.7]
+	for values in range(0,3):
+		distort2=inverted_image
+		for wai in range(0,deltay):
+			for ex in range(strip[values],strip[values]+1):
+				distort2.putpixel((ex,wai),255)
+		bigbox=(x,y,x+deltax,y+deltay)
+		line=akshar+" "+str(bigbox[0])+" "+str(500-(bigbox[1]+deltay))+" "+str(bigbox[2])+" "+str(500-(bigbox[3]-deltay)) # this is the line to be added to the box file
+        	f.write(line+'\n')
+		bigimage.paste(distort2,(x,y))
+		x=bigbox[2]+2
+        	if x>450:
+            		x=10; y=y+35
+	#degrade code ends
      
-        """if(type(original_bbox) != tuple):
-            print "NONETYPE ERROR"
-            line="Nonetype error"
-        else:
-            bbox=expand(original_bbox) #The bounding box should not overlap with the symbol, hence we must expand the boxes by a certain tolerance vale tol.
-            #draw.rectangle(bbox,None,300) #draw the bounding box
-            line=akshar+" "+str(bbox[0])+" "+str(bbox[1])+" "+str(bbox[2])+" "+str(bbox[3])
-            
-        f.write(line+'\n')"""
-        
-        
-        #symbol_image.save(filename,"TIFF")#save the symbol to a file
-        #print filename
-        filecount+=1 # give a new name to the file to be created
-        filename1=image_dir+"/"+"image"+font_name.split('.')[0]+str(filecount)+".png"
-	filename2=image_dir+"/"+"image"+font_name.split('.')[0]+str(filecount)+".tif"
-        #boxfile=image_dir+"/"+"image"+font_name.split('.')[0]+str(filecount)+".box"
-        #f.close()
-        #f=open(boxfile,"w") #open new file
+        #distort.distort(filename2,bbox,fsz,akshar)
+     
+       
+       
     bigimage.save(image_dir+"/"+"bigimage.tif","TIFF")
     f.close()
-        
-            
+       
+           
 if(len(sys.argv)!=7):
     print "Usage: python generate.py -fd <font directory> -l <language> -a <input alphabet directory>"
     exit()
@@ -153,19 +133,19 @@ if(sys.argv[1]=="-fd"):
 else:
     print "Usage: python generate.py -fd <font directory> -l <language> -a <imput alphabet directory>"
     exit()
-        
+       
 if(sys.argv[3]=="-l"):
     lang=sys.argv[4]
 else:
     print "Usage: python generate.py -fd <font directory> -l <language> -a <input alphabet directory>"
     exit()
-    
+   
 if(sys.argv[5]=="-a"):
     alphabet_dir=sys.argv[6]
 else:
     print "Usage: python generate.py -fd <font directory> -l <language> -a <input alphabet directory>"
     exit()
-    
+   
 
 
 #begin training    
@@ -180,3 +160,5 @@ for t in os.walk(font_dir):
 
 train.train(lang)
 #training ends
+
+
