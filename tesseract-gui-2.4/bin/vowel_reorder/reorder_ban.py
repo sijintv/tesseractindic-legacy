806e76cf3cd925e1cfc81da5fc809bfc33e3343d
debayanin <debayanin@8c7c788f-554f-0410-aad6-efc5f5ac1a20>
2009-10-31 07:57:25 +0000

    Adding modifications to branch. The previous commit was made in a confused state of mind

---
+++ tesseract-gui-2.4/bin/vowel_reorder/reorder_ban.py
@@ -0,0 +1,80 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+import sys
+import codecs
+import os
+from optparse import OptionParser
+
+def reorderUnicode(input_text):
+        curr=""
+        next=""
+        prev=""
+        out_text =""
+        index = 0
+        while index < len(input_text):
+                curr = input_text[index:index+3]
+                #print "current="+curr
+                if(index != len(input_text)-1):
+                        next = input_text[index+3:index+6]
+		else: 
+			next = ""
+
+		if(index != len(input_text)-2):
+			next2next = input_text[index+6:index+9]
+                else:
+                        next2next = ""
+#               print "next="+next
+
+		if (curr == "ে" and next2next == "া"):
+			#print "one"
+			curr = next + "ো"
+			index = index+6
+		elif (curr == "ে" and next2next != "্"):
+			#print "two"
+			curr = next + "ে"
+			index = index+3
+		if (curr == "ে" and next2next == "্"):
+			curr = input_text[index+3:index+12] + curr
+			index = index+9
+		if (curr == "ৈ"):
+			curr = next + "ৈ"
+			index = index+3
+		if (curr == "া" ):# and input_text[index+3] == " "):
+			if (len(input_text)-3 == index):
+				pass
+		
+			
+						
+		out_text+=curr
+		prev = curr
+		#print index
+        	index = index+3
+		
+			
+        return out_text
+       
+def readwrite(infile,outfile):
+        f1=open(infile,'r')
+        fc=f1.readlines()
+        f1.close()
+
+        inputs = ""
+        output=""
+        words = []
+        wno=0
+        for ts in fc:
+                words = ts.split(' ')
+                for w in words:
+			output+=reorderUnicode(w)
+                        output+=' '
+                        wno+=1          
+        #print wno
+        f=open(outfile,"w")
+        f.write(output)
+        f.close()
+
+if __name__=="__main__":
+	readwrite(sys.argv[1],sys.argv[2])
+#readwrite('out.txt','data.txt')
+
+
