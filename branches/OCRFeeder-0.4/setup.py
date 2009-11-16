0ef5da109a651dbbd181b041da99fcf1b16f1d9a
debayanin <debayanin@8c7c788f-554f-0410-aad6-efc5f5ac1a20>
2009-11-16 14:47:19 +0000

    Adding OCRFeeder to the repo

---
+++ branches/OCRFeeder-0.4/setup.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+from util import constants
+import glob
+import os
+
+def get_locale_files():
+    files = glob.glob('locale/*/*/*.mo')
+    file_list = []
+    for file in files:
+        file_list.append((os.path.dirname(os.path.join(constants.DEFAULT_SYSTEM_APP_DIR, file)), [file]))
+    return file_list
+
+setup(name = 'OCRFeeder',
+     version = constants.OCRFEEDER_STUDIO_VERSION,
+     description = '''A complete Optical Character Recognition and
+                      Document Analysis and Recognition program.''',
+     author = 'Joaquim Rocha',
+     author_email = 'joaquimrocha1@gmail.com',
+     url = constants.OCRFEEDER_WEBSITE,
+     license = 'GPL v3',
+     packages = ['feeder', 'studio',
+                 'util', 'odf',
+                 ],
+     scripts = ['ocrfeeder', 'ocrfeeder-cli'],
+     data_files = [(constants.DEFAULT_SYSTEM_APP_DIR + 
+                    '/icons', ['resources/icons/detect_icon.svg',
+                               'resources/icons/ocr.svg',
+                               'resources/icons/window_icon.png']
+                   ),
+                   ('/usr/share/icons/hicolor/scalable/apps', ['resources/icons/ocrfeeder.svg']
+                   ),
+                   ('/usr/share/applications', ['resources/ocrfeeder.desktop']
+                   ),
+                   ] + get_locale_files()
+     )
